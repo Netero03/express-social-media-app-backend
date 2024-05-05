6e23c072edb946918761fccc75615192e3875ccb
Netero03 <jatinletsgo@gmail.com>
2024-05-05 22:53:18 +0530

    third commit

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ app.get('/api/qrcode/:userId', async (req, res) => {
 });
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI,{dbName: 'SocialMediaApp', })
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
